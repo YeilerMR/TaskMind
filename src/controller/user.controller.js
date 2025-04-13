273aa223beddb28c4c1bcdd1f49788972aa545db
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-04-13 01:58:30 -0600

    add userServices and modify register_user function

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,65 +1,14 @@
-import { getDateCR } from '../Lib/date.js';
+import { user_Register } from '../logic/user/user.logic.js';
 import User from '../model/user.model.js';
-import { validateRegisterUser, validateUpdateUser } from '../logic/validateField.logic.js';
-import { validateRegister } from '../logic/user/user.logic.js';
-import { encryptData, compareData } from "../Lib/encryptData.js";
+import { encryptData, compareData } from '../Lib/encryptData.js'
+import { validateUpdateUser } from '../logic/validateField.logic.js';
+
 // import { format } from 'date-fns-tz';
 
 
 export const registerUsers = async (req, res) => {
   try {
-    const { DSC_FIRST_NAME, DSC_LAST_NAME_ONE,DSC_IDENTIFICATION, DSC_EMAIL, DSC_PASSWORD, DSC_CAREER, CONFIRM_PASSWORD } = req.body;
-
-    const validateFields = validateRegisterUser(req);
-    if (validateFields !== true) {
-      return res.status(400).json({
-        message: validateFields,
-      })
-    }
-
-    const user = await User.findOne({ where: { DSC_IDENTIFICATION: DSC_IDENTIFICATION } });
-    if (user) {
-      return res.status(404).json({ message: "La cedula se encuentra registrada en el sistema." });
-    }
-
-
-    const output = await validateRegister(DSC_EMAIL);
-    if (output !== true) {
-      return res.status(400).json({
-        message: output,
-      })
-    }
-
-    if (DSC_PASSWORD !== CONFIRM_PASSWORD) {
-      return res.status(400).json({
-        message: 'Las contraseñas no coinciden.',
-      })
-    }
-
-    const passwordHash = await encryptData(DSC_PASSWORD, 10);
-
-    const DATE_CREATED = await getDateCR();
-
-    const newUser = new User({
-      DSC_FIRST_NAME,
-      DSC_LAST_NAME_ONE,
-      DSC_IDENTIFICATION,
-      DSC_EMAIL: DSC_EMAIL.toLowerCase(),
-      DSC_PASSWORD: passwordHash,
-      DATE_CREATED,
-      STATUS: 1,
-      DSC_CAREER,
-    });
-    const userSaved = await newUser.save();
-
-    res.status(200).json({
-      ID_USER: userSaved.DSC_IDENTIFICATION,
-      DSC_FIRST_NAME: userSaved.DSC_FIRST_NAME,
-      DSC_LAST_NAME_ONE: userSaved.DSC_LAST_NAME_ONE,
-      DSC_EMAIL: userSaved.DSC_EMAIL,
-      DSC_CAREER: userSaved.DSC_CAREER,
-      message: 'Registro realizado correctamente.',
-    });
+    user_Register(req, res);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -71,7 +20,7 @@ export const registerUsers = async (req, res) => {
 export const updateUser = async (req, res) => {
   try {
     const {
-      DSC_FIRST_NAME, DSC_LAST_NAME_ONE ,DSC_PASSWORD, CONFIRM_PASSWORD, DSC_CAREER } = req.body;
+      DSC_FIRST_NAME, DSC_LAST_NAME_ONE, DSC_PASSWORD, CONFIRM_PASSWORD, DSC_CAREER } = req.body;
 
     const validateFields = validateUpdateUser(req);
     if (validateFields !== true) {
@@ -113,7 +62,7 @@ export const deleteUser = async (req, res) => {
   try {
 
     const id = req.params.id;
-   
+
     const user = await User.findOne({ where: { DSC_IDENTIFICATION: id } });
     if (!user) {
       return res.status(404).json({ message: "Error al cargar el usuario." });
--- src/logic/user/user.logic.js
+++ src/logic/user/user.logic.js
@@ -1,5 +1,10 @@
 import User from '../../model/user.model.js';
+import { validateRegisterUser, validateUpdateUser } from '../validateField.logic.js';
 import { Op } from 'sequelize';
+import { getDateCR } from '../../Lib/date.js';
+import { userServices } from '../../services/userServices.js';
+import { encryptData, compareData } from '../../Lib/encryptData.js'
+
 
 export const validateRegister = async (DSC_EMAIL) => {
     try {
@@ -26,3 +31,89 @@ async function existEmail(email) {
     return false;
 }
 
+async function existe_Id_User(DSC_IDENTIFICATION) {
+    const user = await User.findOne({ where: { DSC_IDENTIFICATION: DSC_IDENTIFICATION } });
+    return (!user) ? true : "La cedula se encuentra registrada en el sistema.";
+}
+export function validate_Passwords(password, confirmPassword) {
+    return password === confirmPassword;
+}
+
+async function validate_status_user(result, res) {
+
+    if (result && result.DSC_IDENTIFICATION) {
+        return res.status(200).json({
+            ID_USER: result.DSC_IDENTIFICATION,
+            DSC_FIRST_NAME: result.DSC_FIRST_NAME,
+            DSC_LAST_NAME_ONE: result.DSC_LAST_NAME_ONE,
+            DSC_EMAIL: result.DSC_EMAIL,
+            DSC_CAREER: result.DSC_CAREER,
+            message: 'Registro realizado correctamente.',
+        });
+    }
+
+    const errorMessages = {
+        1: 'Error: El email ya está registrado',
+        2: 'Error: Datos inválidos',
+        0: 'Error desconocido',
+    };
+
+
+    return res.status(400).json({ message: errorMessages[result] || 'Error desconocido' });
+}
+
+
+export const user_Register = async (req, res) => {
+    try {
+        const { DSC_FIRST_NAME, DSC_LAST_NAME_ONE, DSC_IDENTIFICATION, DSC_EMAIL, DSC_PASSWORD, DSC_CAREER, CONFIRM_PASSWORD } = req.body;
+
+        const validateFields = validateRegisterUser(req);
+        if (validateFields !== true) {
+            return res.status(400).json({
+                message: validateFields,
+            })
+        }
+
+        const user = await existe_Id_User(DSC_IDENTIFICATION);
+        if (user !== true) {
+            return res.status(400).json({
+                message: user,
+            })
+        }
+
+
+        const output = await validateRegister(DSC_EMAIL);
+        if (output !== true) {
+            return res.status(400).json({
+                message: output,
+            })
+        }
+
+        if (!validate_Passwords(DSC_PASSWORD, CONFIRM_PASSWORD)) {
+            return res.status(400).json({
+                message: 'Las contraseñas no coinciden.',
+            })
+        }
+
+        const passwordHash = await encryptData(DSC_PASSWORD, 10);
+
+        const DATE_CREATED = await getDateCR();
+
+        const newUser = {
+            DSC_FIRST_NAME,
+            DSC_LAST_NAME_ONE,
+            DSC_IDENTIFICATION,
+            DSC_EMAIL: DSC_EMAIL.toLowerCase(),
+            DSC_PASSWORD: passwordHash,
+            DATE_CREATED,
+            STATUS: 1,
+            DSC_CAREER,
+        };
+
+        const userSaved = await userServices.createUser(newUser);
+
+        validate_status_user(userSaved, res)
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
