106a98c0cb08b9af47c82f1cc13b02027ac85bba
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-04-17 12:28:23 -0600

    update controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,4 @@
-import { user_Register } from '../logic/user/user.logic.js';
+import { user_Register, delete_User,update_User } from '../logic/user/user.logic.js';
 import User from '../model/user.model.js';
 import { encryptData, compareData } from '../Lib/encryptData.js'
 import { validateUpdateUser } from '../logic/validateField.logic.js';
@@ -19,38 +19,7 @@ export const registerUsers = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   try {
-    const {
-      DSC_FIRST_NAME, DSC_LAST_NAME_ONE, DSC_PASSWORD, CONFIRM_PASSWORD, DSC_CAREER } = req.body;
-
-    const validateFields = validateUpdateUser(req);
-    if (validateFields !== true) {
-      return res.status(400).json({
-        message: validateFields,
-      })
-    }
-
-    const user = await User.findOne({ where: { DSC_IDENTIFICATION: req.params.id } });
-    if (!user) {
-      return res.status(404).json({ message: "Error al cargar el usuario." });
-    }
-
-    if (DSC_PASSWORD !== CONFIRM_PASSWORD) {
-      return res.status(400).json({
-        message: 'Las contraseñas no coinciden.',
-      })
-    }
-
-    var passwordHash;
-    if (DSC_PASSWORD) {
-      passwordHash = await encryptData(DSC_PASSWORD, 10);
-    } else {
-      passwordHash = user.DSC_PASSWORD;
-    }
-    await user.update({
-      DSC_FIRST_NAME, DSC_LAST_NAME_ONE, DSC_PASSWORD: passwordHash, DSC_CAREER,
-    });
-
-    return res.status(200).json({ message: "Usuario actualizado correctamente" });
+    update_User(req,res);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -60,18 +29,7 @@ export const updateUser = async (req, res) => {
 
 export const deleteUser = async (req, res) => {
   try {
-
-    const id = req.params.id;
-
-    const user = await User.findOne({ where: { DSC_IDENTIFICATION: id } });
-    if (!user) {
-      return res.status(404).json({ message: "Error al cargar el usuario." });
-    }
-
-    user.STATUS = 0;
-    await user.save();
-
-    return res.status(200).json({ message: "Usuario eliminado correctamente" });
+   delete_User(req,res);  
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
