a38e22a62ba5dc2f381b2902b5163899febde141
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-04-17 12:29:01 -0600

    Delete password in updatSchema

--- src/schema/user.schema.js
+++ src/schema/user.schema.js
@@ -96,16 +96,6 @@ export const updateUserSchema = z.object({
       message: "El primer apellido no puede exceder los 50 caracteres.",
     })
     .optional(),
-  DSC_PASSWORD: z
-    .string({
-      required_error: "La contraseña es obligatoria.",
-    })
-    .min(8, {
-      message: "La contraseña debe tener al menos 8 caracteres.",
-    })
-    .max(255, {
-      message: "La contraseña no puede exceder los 255 caracteres.",
-    }),
   DSC_CAREER: z
     .string()
     .min(3, {
