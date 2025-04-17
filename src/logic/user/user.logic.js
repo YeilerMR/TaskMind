8a1a28c69cab920b67c999ffee9c02738d00116c
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-04-17 12:28:44 -0600

    update logic with CRUD functions

--- src/logic/user/user.logic.js
+++ src/logic/user/user.logic.js
@@ -4,6 +4,8 @@ import { Op } from 'sequelize';
 import { getDateCR } from '../../Lib/date.js';
 import { userServices } from '../../services/userServices.js';
 import { encryptData, compareData } from '../../Lib/encryptData.js'
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from '../../config.js';
 
 
 export const validateRegister = async (DSC_EMAIL) => {
@@ -31,10 +33,16 @@ async function existEmail(email) {
     return false;
 }
 
-async function existe_Id_User(DSC_IDENTIFICATION) {
+async function exist_Id_User(DSC_IDENTIFICATION) {
     const user = await User.findOne({ where: { DSC_IDENTIFICATION: DSC_IDENTIFICATION } });
     return (!user) ? true : "La cedula se encuentra registrada en el sistema.";
 }
+
+async function exist_User(DSC_IDENTIFICATION) {
+    const user = await User.findOne({ where: { DSC_IDENTIFICATION: DSC_IDENTIFICATION } });
+    return (user) ? user : "Usuario no encontrado";
+}
+
 export function validate_Passwords(password, confirmPassword) {
     return password === confirmPassword;
 }
@@ -74,7 +82,7 @@ export const user_Register = async (req, res) => {
             })
         }
 
-        const user = await existe_Id_User(DSC_IDENTIFICATION);
+        const user = await exist_Id_User(DSC_IDENTIFICATION);
         if (user !== true) {
             return res.status(400).json({
                 message: user,
@@ -115,5 +123,110 @@ export const user_Register = async (req, res) => {
         validate_status_user(userSaved, res)
     } catch (error) {
         res.status(500).json({ message: error.message });
+    }
+};
+
+
+
+export async function messageDelete(result, res,token) {
+    if (result) {
+       
+
+        if (token) {
+            return jwt.verify(token, TOKEN_SECRET, async (error, user) => {
+                if (error) return res.sendStatus(401);
+
+                const userFound = await User.findOne({ where: { DSC_IDENTIFICATION: user.id } });
+                if (!userFound) return res.sendStatus(401);
+
+                res.cookie("token", "", {
+                    httpOnly: true,
+                    secure: true,
+                    expires: new Date(0),
+                });
+
+                 res.status(200).json({ message: 'Usuario deshabilitado y sesión cerrada correctamente' });
+            });
+        }
+
+    }
+
+     res.status(400).json({ message: 'Error al deshabilitar los usuarios' });
+}
+
+// delete user function
+export const delete_User = async (req, res) => {
+    try {
+        const { token } = req.cookies;
+        const id = req.params.id;
+
+        const user = await exist_User(id);
+        if (typeof user === 'string') {
+            return res.status(400).json({
+                message: user,
+            })
+        }
+
+        const result = await userServices.delectUser(user);
+
+        messageDelete(result, res,token);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+
+function message_Update(result, res) {
+    const response = result
+        ? { status: 200, message: 'Usuario actualizado correctamente' }
+        : { status: 400, message: 'Error al actualizar los usuarios' };
+
+    return res.status(response.status).json({ message: response.message });
+}
+
+export const update_User = async (req, res) => {
+    try {
+        const {
+            DSC_FIRST_NAME, DSC_LAST_NAME_ONE, DSC_PASSWORD, CONFIRM_PASSWORD, DSC_CAREER } = req.body;
+
+        const validateFields = validateUpdateUser(req);
+        if (validateFields !== true) {
+            return res.status(400).json({
+                message: validateFields,
+            })
+        }
+
+        const user = await exist_User(id);
+        if (typeof user === 'string') {
+            return res.status(400).json({
+                message: user,
+            })
+        }
+
+        if (DSC_PASSWORD || CONFIRM_PASSWORD) {
+            if (!validate_Passwords(DSC_PASSWORD, CONFIRM_PASSWORD)) {
+                return res.status(400).json({
+                    message: 'Las contraseñas no coinciden.',
+                })
+            }
+        }
+
+        const passwordHash = DSC_PASSWORD
+            ? await encryptData(DSC_PASSWORD, 10)
+            : user.DSC_PASSWORD;
+
+
+        const newUser = {
+            DSC_FIRST_NAME,
+            DSC_LAST_NAME_ONE,
+            DSC_PASSWORD: passwordHash,
+            DSC_CAREER,
+        };
+
+        const result = await userServices.updateUser(newUser);
+
+        message_Update(result,res);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
 };
